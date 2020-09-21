e7f2be45dad6f5e017e99df851e6aa3d2da8f4f6
littlePuky <zedd@abv.bg>
2020-09-21 09:41:50 +0300

    newexp

--- src/main/java/newBuild.java
+++ src/main/java/newBuild.java
@@ -19,5 +19,8 @@ public class newBuild extends DriverSetup {
     void networkChanges() {
 
         System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 }
